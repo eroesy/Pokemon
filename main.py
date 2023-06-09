28738a0fc47ad32baf67032bbb430c54dad6f8cd
eroesy <131892555+eroesy@users.noreply.github.com>
2023-06-09 10:16:08 -0300

    pokemons

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from time import sleep
 from poke import *
+from os import system
+
+system('cls')
 
 print("Bem vindo ao mundo de pokemon")
-nome = input('Qual o seu nome?: ')
+nome = input('Qual o seu nome?: ').title().strip()
 
 print(f"Ah! Como vai \033[1;33m{nome}\033[m" )
 sleep(1)
@@ -22,6 +25,7 @@ sleep(1)
 print("Vai servir")
 print("Escolhe um ae:")
 iniciais()
+
 
 
 
--- poke/__init__.py
+++ poke/__init__.py
@@ -1,20 +1,38 @@
 
-def cores(cor='limpo'):
+def cores(cor='clear'):
 
     dic ={
-        'azul':'\033[33m',
-        'limpo':'\033[m',
+        'azul':'\033[36m',
+        'clear':'\033[m',
+        'red':'\033[31m',
+        'verd':'\033[32m',
+        'yell':'\033[33m'
 
          }
-    print(dic[cor])
+    return f"{dic[cor]}"
+    
+
+
+def pokemons(poke='bulbasaur'):
+
+    pokers={
+        'bulbasaur':f'{cores("verd")}Bulbasaur{cores()}',
+        'charmander':f'{cores("red")}Charmander{cores()}',
+        'squirtle':f'{cores("azul")}Squirtle{cores()}',
+        'pikachu':f'{cores("yell")}Pikachu{cores()}'
+
+    }
+    return pokers[poke]
+
 
 
 def iniciais():
     print(f"""
-    [1] {cor('azul')}Bulbasaur
-    [2] Charmander
-    [3] Squirtle
-    [4] Pikachu
+    [1] {pokemons()}
+    [2] {pokemons('charmander')}
+    [3] {pokemons('squirtle')}
+    [4] {pokemons('pikachu')}
     """)
-cores('azul'
-print("oie")
+
+print(cores('azul'),"oie",cores())
+
