6cea1f9cd812984cfbf4cb3692b6f8d633a90da1
Matheus <131892555+eroesy@users.noreply.github.com>
2023-06-26 18:10:27 -0300

    mapas

--- poke/__init__.py
+++ poke/__init__.py
@@ -3,13 +3,15 @@ from time import sleep
 def cores(cor='clear'):
 
     dic ={
+        'azul2':'\033[34m',
         'azul':'\033[36m',
         'clear':'\033[m',
         'red':'\033[31m',
         'verd':'\033[32m',
         'yell':'\033[33m',
-        'roxo':'\033[35m]',
-        'cinza':'\033[37m]'
+        'roxo':'\033[35m',
+        'cinza':'\033[37m',
+        'reverse':'\033[7;4m'
         
 
          }
@@ -32,10 +34,10 @@ def dados(pok='bulbasaur'):
     
     pokedex ={
     
-            'bulbasaur': {'Nome':pokemons("bulbasaur"),'Vida': 200, 'Dano':300 , 'Velocidade': 50 ,'Habilidade':'(Cura): Bulbasaur se cura em 5% a cada round', 'Descrição': f'{cores("roxo")}Bulbasaur tem uma variedade de habilidades do tipo Grama que concedidod pelo bulbo crescendo em suas costas. Pós, cheiros, pétalas e sementes podem vir da ponta do bulbo, e vinhas e folhas podem vir da base. Ao manipular-los em movimentos como Semente sanguessuga , Chicote de Vinha, Folha Navalha, Dança de Pétalas, Bomba de Semente, and Doce Incenso, Bulbasaur tem um arsenal muito à sua disposição.{cores("clear")}'},
-            'charmander':{'Nome':pokemons("charmander"),'vida' :150, 'Dano': 300, 'Velocidade': 50,'Descrição':'Charmander é um pequeno dinossauro bípede como pokémon. A maioria da cor do seu corpo é laranja, enquanto o seu ventre baixo é de cor amarelo-claro. Charmander, como suas evoluções, tem uma chama na ponta de sua cauda que arde constantemente.'},
-            'squirtle':{'Nome':pokemons("squirtle"),'vida' :150, 'Dano': 300, 'Velocidade': 50,'Descrição':'O casco de Squirtle não é apenas usado para a proteção. A forma arredondada do casco e as ranhuras em sua superfície ajudam a minimizar a resistência na água, permitindo que este Pokémon nade em altas velocidades.'},
-            'pikachu':{'Nome':pokemons("pikachu"),'vida' :150, 'Dano': 300, 'Velocidade': 50,'Descrição':'Pikachu é um Pokémon roedor meio gordinho, com o pêlo amarelo em todo o seu corpo. As orelhas de Pikachu são longas e chegam a um ponto com pontas pretas. Ele tem uma boca pequena, que se assemelha a um lado tres, tem olhos negros com os alunos brancos e dois círculos vermelhos em suas bochechas. Seus antebraços são curtos e um pouco atarracado, com cinco dedos em cada "mão", e seus pés têm três dígitos. Ele tem duas listras marrons em suas costas, e sua cauda é em forma de raio Zigzag de estilo, com um pedaço de pele marrom na base da cauda. Embora seja tecnicamente um quadrúpede , em várias ocasiões tem sido mostrado para ser capaz de ficar de pé e andar ereto sobre as patas traseiras.'},
+            'bulbasaur': {'Nome':pokemons("bulbasaur"),'Vida': 300, 'Dano':150 , 'Velocidade': 50 ,'Habilidade':'(Cura): Bulbasaur se cura em 5% a cada round', 'Descrição': f'{cores("roxo")}Bulbasaur tem uma variedade de habilidades do tipo Grama que concedidod pelo bulbo crescendo em suas costas. Pós, cheiros, pétalas e sementes podem vir da ponta do bulbo, e vinhas e folhas podem vir da base. Ao manipular-los em movimentos como Semente sanguessuga , Chicote de Vinha, Folha Navalha, Dança de Pétalas, Bomba de Semente, and Doce Incenso, Bulbasaur tem um arsenal muito à sua disposição.{cores("clear")}'},
+            'charmander':{'Nome':pokemons("charmander"),'vida' :150, 'Dano': 300, 'Velocidade': 50,'Descrição':f'{cores("roxo")}Charmander é um pequeno dinossauro bípede como pokémon. A maioria da cor do seu corpo é laranja, enquanto o seu ventre baixo é de cor amarelo-claro. Charmander, como suas evoluções, tem uma chama na ponta de sua cauda que arde constantemente.{cores("clear")}'},
+            'squirtle':{'Nome':pokemons("squirtle"),'vida' :200, 'Dano': 200, 'Velocidade': 50,'Descrição':f'{cores("roxo")}O casco de Squirtle não é apenas usado para a proteção. A forma arredondada do casco e as ranhuras em sua superfície ajudam a minimizar a resistência na água, permitindo que este Pokémon nade em altas velocidades.{cores("clear")}'},
+            'pikachu':{'Nome':pokemons("pikachu"),'vida' :150, 'Dano': 300, 'Velocidade': 50,'Habilidade':f'(Paralisia): {pokemons("pikachu")} tem 5% de chance de paralisar o oponente(paralisia dura {cores("cinza")}3{cores()} turnos)  ','Descrição':f'{cores("roxo")}Pikachu é um Pokémon roedor meio gordinho, com o pêlo amarelo em todo o seu corpo. A sua cauda é em forma de raio Zigzag de estilo, com um pedaço de pele marrom na base da cauda. Embora seja tecnicamente um quadrúpede , em várias ocasiões tem sido mostrado para ser capaz de ficar de pé e andar ereto sobre as patas traseiras.{cores("clear")}'},
 
                  
                  
@@ -63,7 +65,7 @@ def ler(texto='vai se fuder giselly liege: ', modo ='int'):
             except (ValueError, TypeError):
                 print(f'{cores("red")}ERROR!!! Digite um numero inteiro.{cores("clear")}')
             except(KeyboardInterrupt):
-                print(f'{cores["red"]}ERROR!!! Usuario não digitou um numero.{cores("clear")}')
+                print(f'{cores("red")}ERROR!!! Usuario não digitou um numero.{cores("clear")}')
             else:
                 break
         
@@ -84,7 +86,25 @@ def ler(texto='vai se fuder giselly liege: ', modo ='int'):
                 
 
 
+def mapas(mapa= 'floresta'):
+    
+    mapas = {
+        'floresta': f'A{cores("verd")} floresta {cores()}cheia de pokemons maravilhosos. Um otimo lugar para um noob!\nDifilcudade: {cores("azul")} FACIL {cores()}',
+        'montanha': f'A {cores("yell")}MONTANHA{cores()} Lar de pokemons pedras e voadores extraordinarios,Cuidado com os {cores("roxo")}"Cães"{cores()}.\nDifilcudade: {cores("azul2")}MEDIA {cores()}',
+        'deserto': f'O{cores("yell")}DESERTO{cores("clear")}, Casa dos pokemons nordestinos, secas e vento forte são do contidiano dos moradores({cores("roxo")}VEM PRO NORDESTE MADONA({cores()})).\nDifilcudade: {cores("red")}DIFICIL{cores()}',
+        'HELL': f'O {cores("roxo")}Lugar assombradooooo{cores("clear")}\n\n"Se você ja morreu alguma vez sua alma não me pertence"\n {cores("red")}                                        - O tinhoso xxx\n\n{cores("clear")}\nDifilcudade: {cores("red")}VOCê JA ESTA MORTO, NÃO TEM COMO PIORAR ><.{cores()}',
+        'DREAMCORE': f'{cores("reverse")}Como aliviar sua dor, ficar no Dreamcore?{cores("clear")}(Não a Informação do local OBS: {cores("red")}NIGUEM NUNCA VOLTOU!!!!){cores("clear")}\nDifilcudade: {cores("reverse")}Talvez não seja tão ruim assim?{cores()}'}
 
+    for i in mapas[mapa]:
+        print(i,end="")
+        
+        
+    
+
+def aventura(local='floresta'):
+    print('Voce finalmente esta pronto(a) para começar a sua AVENTURA!')
+    print('Escolha um lugar para começar: ')
+    mapas()
     
 
     
@@ -101,6 +121,8 @@ def iniciais():
             opç = str(ler('Qual vai ser a opção?: ',modo='string'))
             if opç in '1234':
                 break
+            if opç == 'VSFD' or 'vsfd':
+                print(f'{cores("red")}VAI SE FUDER VOCÊ \n LIXO, GABIRU, TIMBU, ESCAMBO.{cores("clear")}')
             print('Somente as opçôes mostradas estão validas!')
         
         
@@ -121,6 +143,7 @@ def iniciais():
         
     
     print('OK.')
+    aventura()
        
    
-iniciais()
+mapas('HELL')
